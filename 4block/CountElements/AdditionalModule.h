c86e35799f9c6a84c6bfa6665d0bb28a1b322fcb
lilpasshtet <54393273+lilpasshtet@users.noreply.github.com>
2020-10-14 19:30:10 +0300

    Delete AdditionalModule.h

--- 4block/CountElements/AdditionalModule.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-void ownQuickSort(int *arrayElement, const int leftElementIndex, const int rightElementIndex);
-int sizeArray(char *inputFile);
-void createArray(int *arrayOfElements, const int sizeOfArray, char *inputFile);
